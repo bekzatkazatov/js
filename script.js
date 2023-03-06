a7093fbe86386195f7211e8c7f7f2e3cffa7b5ec
bekzatkazatov <bekzatx2x@gmail.com>
2023-03-06 14:08:22 +0600

    jst

---
+++ script.js
@@ -0,0 +1,34 @@
+
+// let a = 4
+//
+// let b = 55
+//
+// console.log(a + b)   59
+// console.log(a * b)   220
+// console.log(a / b)   0,072
+// console.log(a % b)    4
+
+// let a = 'hello'
+// let b = 'medet'
+// console.log(a + b)   hellomedet
+
+// let a = 0
+// let b = '0'
+// console.log(typeof a*b)  NaN
+// {
+// let a = 5
+// let b = '5'
+// console.log ( +b + a)     10
+
+// let a = 5
+// let b = '5'
+// console.log(Number(b)+a)    10
+
+// let a = 5
+// let b = '5'
+// console.log(a+b-45)      10
+// }
+
+// let c = 2
+// let b = undefined
+// console.log(typeof b)  undefined
